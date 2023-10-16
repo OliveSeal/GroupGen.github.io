b5f4da4b095a39b14427b3af1d60048ca5aacaa3
OliveSeal <oliverplayz34@gmail.com>
2023-10-16 12:50:02 +0200

    simplified2

--- js.js
+++ js.js
@@ -1,38 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const generateButton = document.getElementById("generateButton");
   const peopleTextarea = document.getElementById("people");
   const numGroupsInput = document.querySelector(".numgroups");
   const outputDiv = document.getElementById("output");
-  
-  generateButton.addEventListener("click", function () {
+
+  generateButton.addEventListener("click", () => {
     const numGroups = parseInt(numGroupsInput.value);
-    const peopleList = peopleTextarea.value.split('\n').filter(person => person.trim() !== '');
+    const peopleList = peopleTextarea.value.trim().split('\n').filter(Boolean);
 
-    outputDiv.innerHTML = ''; // Clear previous results
-
+    outputDiv.innerHTML = '';
+    
     if (isNaN(numGroups) || numGroups <= 0 || peopleList.length < numGroups) {
       outputDiv.innerHTML = "Invalid input. Please check the number of groups and people.";
       return;
     }
 
-    // Shuffle the list of people
-    for (let i = peopleList.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [peopleList[i], peopleList[j]] = [peopleList[j], peopleList[i]];
-    }
+    shuffleArray(peopleList);
 
-    // Split the shuffled list into groups
     const groups = Array.from({ length: numGroups }, () => []);
 
-    peopleList.forEach((person, index) => {
-      groups[index % numGroups].push(person);
-    });
+    peopleList.forEach((person, index) => groups[index % numGroups].push(person));
 
-    // Display the groups
-    groups.forEach((group, index) => {
-      const groupDiv = document.createElement("div");
-      groupDiv.innerHTML = `<h3>Group ${index + 1}:</h3><ul>${group.map(person => `<li>${person}</li>`).join("")}</ul>`;
-      outputDiv.appendChild(groupDiv);
-    });
+    outputDiv.innerHTML = groups.map((group, index) => `
+      <div>
+        <h3>Group ${index + 1}:</h3>
+        <ul>${group.map(person => `<li>${person}</li>`).join("")}</ul>
+      </div>
+    `).join("");
   });
 });
+
+function shuffleArray(array) {
+  array.sort(() => Math.random() - 0.5);
+}
